057e3ac4b0fc8b81eb93cccf45cced0da4286388
Ebifiri <ebifiri.ikolispiff.chsi@gmail.com>
2026-01-15 01:46:11 +0100

    added warehouse staff, API not implemented yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,27 @@
+<<<<<<< Updated upstream
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import OnboardingFlow from "./pages/OnboardingFlow";
 import StaffDashboard from "./pages/StaffDashboard";
+=======
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from "react-router-dom";
+import OnboardingFlow from "./pages/OnboardingFlow";
+import Dashboard from "./pages/warehouse/Dashboard";
+import ProductPage from "./pages/warehouse/productPage";
+import OrderSummary from "./pages/warehouse/orderSummary";
+import Pickup from "./pages/warehouse/pickup";
+import { CartProvider } from "./context/CartContext";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <>
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/products" element={<ProductPage />} />
+    <Route path="/order-summary" element={<OrderSummary />} />
+    <Route path="/pickup" element={<Pickup />} />
+  </>
+  )
+)
+>>>>>>> Stashed changes
 
 function App() {
   return (
